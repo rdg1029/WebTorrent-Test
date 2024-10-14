b856ee99a3a1ad3cccbf245de9c27f6c8907b184
wcle3456 <wcle3456@gmail.com>
2024-10-14 06:54:43 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,29 +4,29 @@ import WebTorrent from "webtorrent";
 const client = new WebTorrent()
 
 client.on('error', err => {
-    log('ERROR: ' + err)
-    console.error('ERROR: ' + err)
+    log('ERROR: ' + err);
+    console.error('ERROR: ' + err);
 })
 
 document.querySelector('form')?.addEventListener('submit', e => {
-    e.preventDefault() // Prevent page refresh
+    e.preventDefault(); // Prevent page refresh
 
-    const torrentId = document.querySelector('form input[name=torrentId]') as HTMLInputElement
-    log('Adding ' + torrentId.value)
-    client.add(torrentId.value, onTorrent)
+    const torrentId = document.querySelector('form input[name=torrentId]') as HTMLInputElement;
+    log('Adding ' + torrentId.value);
+    client.add(torrentId.value, onTorrent);
 })
 
 async function onTorrent(torrent) {
     console.log(torrent);
-    log('Got torrent metadata!')
-    log(`metadata: ${torrent.metadata}`)
-    log(`magnetURI: ${torrent.magnetURI}`)
-    log(`announce: ${torrent.announce}`)
+    log('Got torrent metadata!');
+    log(`metadata: ${torrent.metadata}`);
+    log(`magnetURI: ${torrent.magnetURI}`);
+    log(`announce: ${torrent.announce}`);
     log(
         'Torrent info hash: ' + torrent.infoHash + ' ' +
         '<a href="' + torrent.magnetURI + '" target="_blank">[Magnet URI]</a> ' +
         '<a href="' + URL.createObjectURL(torrent.torrentFileBlob) + '" target="_blank" download="' + torrent.name + '.torrent">[Download .torrent]</a>'
-    )
+    );
 
     // Print out progress every 5 seconds
     // const interval = setInterval(() => {
@@ -34,27 +34,27 @@ async function onTorrent(torrent) {
     // }, 5000)
 
     torrent.on('download', (bytes) => {
-        console.log('just downloaded: ' + bytes)
-        console.log('total downloaded: ' + torrent.downloaded)
-        console.log('download speed: ' + torrent.downloadSpeed)
-        console.log('progress: ' + torrent.progress)
-      })
+        console.log('just downloaded: ' + bytes);
+        console.log('total downloaded: ' + torrent.downloaded);
+        console.log('download speed: ' + torrent.downloadSpeed);
+        console.log('progress: ' + torrent.progress);
+      });
 
     torrent.on('done', () => {
-        log('done')
-        // clearInterval(interval)
-    })
+        log('done');
+        // clearInterval(interval);
+    });
 
     // Render all files into to the page
     for (const file of torrent.files) {
         try {
-            const blob = await file.blob()
-            document.querySelector('.log')?.append(file.name)
-            log('(Blob URLs only work if the file is loaded from a server. "http//localhost" works. "file://" does not.)')
-            log('File done.')
-            log('<a href="' + URL.createObjectURL(blob) + '">Download full file: ' + file.name + '</a>')
+            const blob = await file.blob();
+            document.querySelector('.log')?.append(file.name);
+            log('(Blob URLs only work if the file is loaded from a server. "http//localhost" works. "file://" does not.)');
+            log('File done.');
+            log('<a href="' + URL.createObjectURL(blob) + '">Download full file: ' + file.name + '</a>');
         } catch (err) {
-            if (err) log(err.message)
+            if (err) log(err.message);
         }
     }
 }
@@ -63,14 +63,14 @@ const fileInput = document.getElementById('file-input') as HTMLInputElement
 fileInput.addEventListener('input', () => {
     const files = fileInput.files;
     if (!files) return;
-    log('file uploaded!')
-    client.seed(files, onTorrent)
+    log('file uploaded!');
+    client.seed(files, onTorrent);
 })
 
 function log(str) {
-    const p = document.createElement('p')
-    p.innerHTML = str
-    document.querySelector('.log')?.appendChild(p)
+    const p = document.createElement('p');
+    p.innerHTML = str;
+    document.querySelector('.log')?.appendChild(p);
 }
 
 // const iceConfig = {
